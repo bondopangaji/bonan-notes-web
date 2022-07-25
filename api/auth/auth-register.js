4e4251b2b09ccd4fc90f12a4142a4e46d4b12d02
bondopangaji <60285086+bondopangaji@users.noreply.github.com>
2022-07-25 17:38:04 +0700

    feat(database): add update functionality and change db strat to rtdb

--- api/auth/auth-register.js
+++ api/auth/auth-register.js
@@ -4,14 +4,14 @@
 // https://opensource.org/licenses/MIT
 
 // Register Event Handler
-const registerForm = document.getElementById("register-form")
+const registerForm = document.getElementById("register-form");
 registerForm.addEventListener("submit", (e) => {
-  e.preventDefault()
-  const name = registerForm["name"].value
-  const email = registerForm["email"].value
-  const password = registerForm["password"].value
-  registerForm.reset()
-  register(name, email, password)
+  e.preventDefault();
+  const name = registerForm["name"].value;
+  const email = registerForm["email"].value;
+  const password = registerForm["password"].value;
+  registerForm.reset();
+  register(name, email, password);
 });
 
 // Register Method
@@ -19,23 +19,11 @@ function register(name, email, password) {
   auth
     .createUserWithEmailAndPassword(email, password)
     .then((cred) => {
-      return fs
-        .collection("users")
-        .doc(cred.user.uid)
-        .set({
-          Name: name,
-          Email: email,
-          Password: password,
-        })
-        .then(() => {
-          location = "index.html";
-        })
-        .catch((err) => {
-          alert(err.message)
-        });
+      cred.updateProfile({
+        displayName: name,
+      });
     })
     .catch((err) => {
-      alert(err.msg)
+      alert(err.msg);
     });
 }
-
--- api/notes/notes.js
+++ api/notes/notes.js
@@ -7,12 +7,11 @@
 const form = document.getElementById("form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const id = uuidv4();
   const noteTitle = form["note-title"].value;
   const noteDescription = form["note-description"].value;
   const createdAt = dateNow();
   form.reset();
-  addNote(id, noteTitle, noteDescription, createdAt)
+  addNote(noteTitle, noteDescription, createdAt);
 });
 
 /**
@@ -22,126 +21,148 @@ form.addEventListener("submit", (e) => {
  * @param noteDescription
  * @param createdAt
  */
-function addNote(id, noteTitle, noteDescription, createdAt) {
-  if (id === null && false) {
-    alert("User id doesn't exist!")
-  } else if (noteTitle === null && false) {
-    alert("Please fill notes-title");
-  } else if (noteDescription === null && false) {
-    alert("Please fill notes-description");
-  } else {
-    auth.onAuthStateChanged((user) => {
-      if (user) {
-        fs.collection(user.uid)
-          .doc("_" + id)
-          .set({
-            id: "_" + id,
-            noteTitle,
-            noteDescription,
-            createdAt
-          })
-          .then(() => {
-            alert("Successfully added notes!");
-          })
-          .catch((err) => {
-            alert(err.message);
-          });
-      }
-    });
-  }
+function addNote(noteTitle, noteDescription, createdAt) {
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      db.ref("users/" + user.uid + "/notes").push(
+        {
+          title: noteTitle,
+          description: noteDescription,
+          createdAt: createdAt,
+        },
+        (error) => {
+          if (error) {
+            alert(error);
+          } else {
+            location.reload();
+          }
+        }
+      );
+    }
+  });
 }
 
 // Container for each note
-const noteContainer = document.getElementById("cards")
+const noteContainer = document.getElementById("cards");
 
 /**
- * Method to ender note
- * @param individualDoc
+ * Method to render note
+ * @param {*} childKey
+ * @param {*} childData
  */
-function getAll(individualDoc) {
+function getAll(childKey, childData) {
   // Parent element
-  const parentEl = document.createElement("li")
-  parentEl.className = "cards_item"
-  parentEl.setAttribute("data-id", individualDoc.id)
+  const parentEl = document.createElement("li");
+  parentEl.className = "cards_item";
+  parentEl.setAttribute("data-id", childKey);
   // Card
-  const card = document.createElement("div")
-  card.className = "card"
+  const card = document.createElement("div");
+  card.className = "card";
   // Card Content
-  const cardContent = document.createElement("div")
-  cardContent.className = "card_content"
+  const cardContent = document.createElement("div");
+  cardContent.className = "card_content";
+  cardContent.dataset.target = "modal-update";
+  cardContent.setAttribute("onClick", "toggleModal(event)");
+  cardContent.setAttribute("data-id", childKey);
   // Card title
-  const cardTitle = document.createElement("h2")
-  cardTitle.className = "card_title"
-  cardTitle.textContent = individualDoc.data().noteTitle
+  const cardTitle = document.createElement("h2");
+  cardTitle.className = "card_title";
+  cardTitle.textContent = childData.title;
   // Card description
-  const cardDescription = document.createElement("p")
-  cardDescription.className = "card_text"
-  cardDescription.textContent = individualDoc.data().noteDescription
+  const cardDescription = document.createElement("p");
+  cardDescription.className = "card_text";
+  cardDescription.textContent = childData.description;
   // Card date
-  const cardDate = document.createElement("small")
-  cardDate.className = "card_date"
-  cardDate.textContent = individualDoc.data().createdAt
+  const cardDate = document.createElement("small");
+  cardDate.className = "card_date";
+  cardDate.textContent = childData.createdAt;
   // Delete button
-  let delBtn = document.createElement("a")
-  delBtn.className = "del-btn"
+  let delBtn = document.createElement("a");
+  delBtn.className = "del-btn";
 
   // Append elements
-  card.appendChild(delBtn)
-  cardContent.appendChild(cardTitle)
-  cardContent.appendChild(cardDescription)
-  cardContent.appendChild(cardDate)
-  card.appendChild(cardContent)
-  parentEl.appendChild(card)
-  noteContainer.appendChild(parentEl)
+  card.appendChild(delBtn);
+  cardContent.appendChild(cardTitle);
+  cardContent.appendChild(cardDescription);
+  cardContent.appendChild(cardDate);
+  card.appendChild(cardContent);
+  parentEl.appendChild(card);
+  noteContainer.appendChild(parentEl);
+
+  // Update note event handler
+  const formUpdate = document.getElementById("form-update");
+  cardContent.addEventListener("click", (e) => {
+    const key = e.target.parentElement.getAttribute("data-id");
+    console.log(key);
+    formUpdate.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const data = {
+        title: formUpdate["note-title-update"].value,
+        description: formUpdate["note-description-update"].value,
+      };
+      form.reset();
+      updateNote(key, data);
+      console.log(key, data);
+    });
+  });
 
   // Remove note event handler
   delBtn.addEventListener("click", (e) => {
     const id = e.target.parentElement.parentElement.getAttribute("data-id");
-    // Check if note exist
-    if (id === null && false) {
-      alert("Note with id: " + id + " does not exist");
-    } else {
-      removeNote(id);
+    removeNote(id);
+  });
+}
+
+/**
+ * Method to update note
+ * @param key
+ */
+function updateNote(key, data) {
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      db.ref("users/" + user.uid + "/notes")
+        .child(key)
+        .update(data);
     }
+    location.reload();
   });
 }
 
 /**
  * Method to remove note
- * @param id
+ * @param key
  */
-function removeNote(id) {
+function removeNote(key) {
   auth.onAuthStateChanged((user) => {
     if (user) {
-      fs.collection(user.uid)
-        .doc(id)
-        .delete()
-        .then(() => {
-          console.log("Document successfully deleted!");
-        })
-        .catch((error) => {
-          console.error("Error removing document: ", error);
-        });
+      db.ref("users/" + user.uid + "/notes")
+        .child(key)
+        .remove();
     }
+    location.reload();
   });
 }
 
-/**
- * Snapshot listener
- */
 auth.onAuthStateChanged((user) => {
   if (user) {
-    fs.collection(user.uid).onSnapshot((snapshot) => {
-      let changes = snapshot.docChanges();
-      changes.forEach((change) => {
-        if (change.type === "added") {
-          getAll(change.doc);
-        } else if (change.type === "removed") {
-          let li = noteContainer.querySelector(
-            "[data-id=" + change.doc.id + "]"
-          );
-          noteContainer.removeChild(li);
-        }
+    let notesRef = db.ref("users/" + user.uid);
+
+    notesRef.on("child_added", (snapshot) => {
+      // const values = snapshot.val();
+      snapshot.forEach((childSnapshot) => {
+        let childKey = childSnapshot.key;
+        let childData = childSnapshot.val();
+        console.log(childSnapshot);
+        getAll(childKey, childData);
+      });
+    });
+
+    notesRef.on("child_removed", (snapshot) => {
+      // const values = snapshot.val();
+      snapshot.forEach((childSnapshot) => {
+        let childKey = childSnapshot.key;
+        let li = noteContainer.querySelector("[data-id=" + childKey + "]");
+        noteContainer.removeChild(li);
       });
     });
   }
